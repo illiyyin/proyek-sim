4ebbb1b1a7426da881157dfd43550548419019d3
oliviadian <oliviadiaan12@gmail.com>
2019-11-03 13:57:15 +0700

    addnotes & detailcourses

---
+++ resources/views/addnotes.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h2>Add Courses</h2>
+        <div class="card">
+            <div class="card-body">
+            <a href="/detailcourses">+ Click here to add your courses </a>
+            </div>
+        </div>
+        <br>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/detailcourses', function () {
+    return view('detailcourses');
+});
